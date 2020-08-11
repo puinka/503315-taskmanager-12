3babcbf4b5c8c14ad3aca4e0b52553f28317d359
Valeria <vozzheva@mail.ru>
2020-08-11 16:00:24 +0300

    Описывает структуру таска

---
+++ src/mock/task.js
@@ -0,0 +1,13 @@
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const generateDescription = () => {
+  const descriptions = [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`
+  ];

b9e15b47e8ecd7f3d7ab27e0cf461f7cb89d86cf
Valeria <vozzheva@mail.ru>
2020-08-11 16:04:11 +0300

    Генерирует рандомную дату

--- src/mock/task.js
+++ src/mock/task.js
@@ -11,3 +11,48 @@ const generateDescription = () => {
     `Сделать домашку`,
     `Пройти интенсив на соточку`
   ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
+const generateDate = () => {
+
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+
+  currentDate.setHours(23, 59, 59, 999);
+
+  currentDate.setDate(currentDate.getDate() + daysGap);
+
+  return new Date(currentDate);
+};
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating: {
+      mo: false,
+      tu: false,
+      we: false,
+      th: false,
+      fr: false,
+      sa: false,
+      su: false
+    },
+    color: `black`,
+    isArchive: false,
+    isFavorite: false
+  };
+};

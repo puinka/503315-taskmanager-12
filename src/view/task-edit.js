8cb63418b39d316c66aa2beab82831bdd2f7e079
Valeria <vozzheva@mail.ru>
2020-08-18 21:27:24 +0300

    Выносит функцию, генерирующую дни повторений, вне шаблона

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -19,25 +19,32 @@ const createTaskEditDateTemplate = (dueDate) => {
   `;
 };
 
+const createRepeatingDaystemlate = (repeating) => {
+  return Object.entries(repeating).map(([day, repeat]) => `<input
+          class="visually-hidden card__repeat-day-input"
+          type="checkbox"
+          id="repeat-${day}"
+          name="repeat"
+          value="${day}"
+          ${repeat ? `checked` : ``}
+          />
+          <label class="card__repeat-day" for="repeat-${day}"
+          >${day}</label
+          >`).join(``);
+};
+
 const createTaskEditRepeatingTemplate = (repeating) => {
-  return `<button class="card__repeat-toggle" type="button">
-    repeat:<span class="card__repeat-status">${isTaskRepeating(repeating) ? `yes` : `no`}</span>
-  </button>
-  ${isTaskRepeating(repeating) ? `<fieldset class="card__repeat-days">
+  let repetingDaysTemplate = `<button class="card__repeat-toggle" type="button">
+  repeat:<span class="card__repeat-status">${isTaskRepeating(repeating) ? `yes` : `no`}</span>
+</button>`;
+  if (isTaskRepeating(repeating)) {
+    repetingDaysTemplate += `<fieldset class="card__repeat-days">
     <div class="card__repeat-days-inner">
-      ${Object.entries(repeating).map(([day, repeat]) => `<input
-        class="visually-hidden card__repeat-day-input"
-        type="checkbox"
-        id="repeat-${day}"
-        name="repeat"
-        value="${day}"
-        ${repeat ? `checked` : ``}
-      />
-      <label class="card__repeat-day" for="repeat-${day}"
-        >${day}</label
-      >`).join(``)}
+      ${createRepeatingDaystemlate(repeating)}
     </div>
-  </fieldset>` : ``}`;
+  </fieldset>`;
+  }
+  return repetingDaysTemplate;
 };
 
 const createTaskEditColorsTemplate = (currentColor) => {
